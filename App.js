6bb381cfd2a111f33bf0377bad05f928100640c1
gianlucaparadise <gianlucaparadise@gmail.com>
2019-08-27 13:44:19 +0200

    Issue repro and readme

--- App.js
+++ App.js
@@ -1,114 +1,92 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow
  */
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, SafeAreaView, Animated, Easing } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+	image2AnimatedValue = new Animated.Value(0);
+	image1AnimatedValue = new Animated.Value(0);
+
+	componentDidMount() {
+		this.animate()
+	}
+
+	animate() {
+		this.image1AnimatedValue.setValue(0);
+		const image1Timing = Animated.timing(
+			this.image1AnimatedValue,
+			{
+				toValue: 1,
+				duration: 1000,
+				easing: Easing.linear,
+				useNativeDriver: true,
+			}
+		);
+
+		this.image2AnimatedValue.setValue(0);
+		const image2Timing = Animated.timing(
+			this.image2AnimatedValue,
+			{
+				toValue: 1,
+				duration: 300,
+				easing: Easing.exp,
+				useNativeDriver: true,
+			}
+		);
+
+		Animated.sequence([image1Timing, image2Timing]).start();
+	}
+
+	render() {
+		//#region Image1: Fade animation
+		const fade = this.image1AnimatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0, 1]
+		});
+
+		const image1Style = { opacity: fade };
+    	//#endregion
+    
+		//#region Image2: Fade+Rotation animation
+		const rotateX = this.image2AnimatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['90deg', '0deg']
+		});
+
+		const opacity = this.image2AnimatedValue.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [0, 1, 1]
+		});
+
+		// FIXME: rotateX doesn't work on iOS, I don't know why
+		const image2Style = { opacity, transform: [{ rotateX }] };
+		//#endregion
+
+		return (
+			<SafeAreaView style={styles.container}>
+        		<Animated.Image resizeMode="contain" style={[styles.logo, image1Style]} source={require('./resources/images/puppy.png')} ></Animated.Image>
+        		<Animated.Image resizeMode="contain" style={[styles.logo, image2Style]} source={require('./resources/images/poop.png')} ></Animated.Image>
+			</SafeAreaView>
+		);
+	}
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		width: '100%',
+		height: '100%',
+	},
+	logoContainer: {
+	},
+	logo: {
+		width: '50%',
+		height: undefined,
+		aspectRatio: 1,
+	},
 });
-
-export default App;
